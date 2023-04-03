a55464801237c3f82ce82b94329223ab0ca00a2f
GauravAde-git <126457771+GauravAde-git@users.noreply.github.com>
2023-04-03 20:51:09 +0530

    Add files via upload

---
+++ JbkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbkProjectApplication.class, args);
+	}
+
+}
